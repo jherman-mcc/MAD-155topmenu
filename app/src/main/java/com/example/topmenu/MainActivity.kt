ccc1507387c0685e5aefff4e13fc6b2a05a3bd5a
JH <https://github.com/jherman-mcc/MAD-105aboutme.git>
2021-10-05 23:38:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/topmenu/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.topmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+
+    }
+}
